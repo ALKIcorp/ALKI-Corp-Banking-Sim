8707f9bc41aafc1d78f04f9ca72b17ba38613449
ALKI corp. <alkibiades.corp@gmail.com>
2026-02-05 14:24:51 -0500

    Fixed applitions of mortgages

--- src/main/java/com/alkicorp/bankingsim/repository/MortgageRepository.java
+++ src/main/java/com/alkicorp/bankingsim/repository/MortgageRepository.java
@@ -4,11 +4,14 @@ import com.alkicorp.bankingsim.model.Mortgage;
 import com.alkicorp.bankingsim.model.enums.MortgageStatus;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MortgageRepository extends JpaRepository<Mortgage, Long> {
+    @EntityGraph(attributePaths = { "product" })
     List<Mortgage> findBySlotId(int slotId);
 
+    @EntityGraph(attributePaths = { "product" })
     List<Mortgage> findBySlotIdAndUserId(int slotId, Long userId);
 
     List<Mortgage> findByClientId(Long clientId);
